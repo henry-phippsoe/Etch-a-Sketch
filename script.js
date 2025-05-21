cf1e4cbecafdcd5fd5ab2378447fe96a12fa1f6f
cowlyfe <26hsoeiro@cpsd.us>
2025-05-21 17:05:54 -0400

    added code that waits for DOM to finish loading so container actually exists and doesnt try to run null

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const container = document.querySelector(".container");
-const num = (400 / 16) ** 2;
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".container");
+    const num = (400 / 16) ** 2;
 
-for (let i = 0; i < num; i++) {
-    container.appendChild((document.createElement('div')).classList.add(".griditem"));
-}
+    for (let i = 0; i < num; i++) {
+        const div = document.createElement('div');
+        div.classList.add("griditem");
+        container.appendChild(div);
+    }
+});

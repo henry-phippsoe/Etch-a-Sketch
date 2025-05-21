78fde5b2a1dc324145c0e2dea966ec7caa8a9d67
henry-phippsoe <26hsoeiro@cpsd.us>
2025-05-21 13:37:52 -0400

    added code that is bad and doesnt work

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const container = document.querySelector(".container");
-const square = 16;
-const box = 400;
 const num = (400 / 16) ** 2;
 
 for (let i = 0; i < num; i++) {
-    
+    container.appendChild((document.createElement('div')).classList.add(".griditem"));
 }

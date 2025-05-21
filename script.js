3a9ec2f1dbb5c5b03e11db2528822559678ab2ba
cowlyfe <26hsoeiro@cpsd.us>
2025-05-21 18:07:33 -0400

    broken things

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container");
-    const num = (400 / 16) ** 2;
+function generateNew() {
+    document.addEventListener("DOMContentLoaded", () => {
 
-    for (let i = 0; i < num; i++) {
-        const div = document.createElement('div');
-        div.id = "griditem";
-        div.addEventListener("mouseenter", () => {
-            div.style.backgroundColor = "black";
-        });
-        container.appendChild(div);
-    }
+        const container = document.querySelector(".container");
+        const griditem = document.querySelector(".pxsize")
 
-});
+        const dimensions = prompt("Please input dimensions: ");
+        const pixel = parseInt(dimensions) / 25;
+        const totalPixels = (parseInt(dimensions) / pixel) ** 2;
+
+        console.log(pixel);
+        console.log(totalPixels);
+
+        container.style.width = "100px"
+        container.style.height = "100px"
+        griditem.style.width = "100px"
+        griditem.style.height = "100px"
+
+
+
+        for (let i = 0; i < totalPixels; i++) {
+            const div = document.createElement('div');
+            div.classList.add("pxsize");
+            div.id = "griditem";
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = "black";
+            });
+            container.appendChild(div);
+        }
+    });
+}
+
+generateNew();

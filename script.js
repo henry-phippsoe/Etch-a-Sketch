9b1f7e88ee9fe6f720d5ab5cf9f7a9856f879a7c
henry-phippsoe <26hsoeiro@cpsd.us>
2025-05-27 13:22:50 -0400

    useless func gone

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const container = document.querySelector(".container");
-const gridItem = document.getElementById("griditem");
-const num = (400 / 16) ** 2;
+function newBoard() {
+    const container = document.querySelector(".container");
+    const gridItem = document.getElementById("griditem");
+    const num = (400 / 16) ** 2;
 
-for (let i = 0; i < num; i++) {
-    const div = document.createElement('div');
-    div.id = "griditem";
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "black";
-    });
-    container.appendChild(div);
+    for (let i = 0; i < num; i++) {
+        const div = document.createElement('div');
+        div.id = "griditem";
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "black";
+        });
+        container.appendChild(div);
+    }
+
+    
+
 }

81e7e946ba9632337b7518fd6ef1514ec050ccc9
henry-phippsoe <26hsoeiro@cpsd.us>
2025-05-27 13:17:58 -0400

    the dom thing was useless in hindsight

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container");
-    const num = (400 / 16) ** 2;
+const container = document.querySelector(".container");
+const gridItem = document.getElementById("griditem");
+const num = (400 / 16) ** 2;
 
-    for (let i = 0; i < num; i++) {
-        const div = document.createElement('div');
-        div.id = "griditem";
-        div.addEventListener("mouseenter", () => {
-            div.style.backgroundColor = "black";
-        });
-        container.appendChild(div);
-    }
-
-});
+for (let i = 0; i < num; i++) {
+    const div = document.createElement('div');
+    div.id = "griditem";
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "black";
+    });
+    container.appendChild(div);
+}

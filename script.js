d5a87f33318c4ade2d95d6fef522d1ec4a8897f5
henry-phippsoe <26hsoeiro@cpsd.us>
2025-05-28 12:40:02 -0400

    OH MY GOD IT WORKS

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 function newBoard(numSquares) {
     const container = document.querySelector(".container");
-    const gridItem = document.getElementById("griditem");
-    const sqsize = (400 / numSquares);
-    const num =  sqsize ** 2;
+    const gridItem = document.getElementById("griditem")
 
     container.innerHTML = '';
 
-    console.log(num);
-    console.log(window.getComputedStyle(container).getPropertyValue('width'));
-
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < numSquares ** 2; i++) {
         const div = document.createElement('div');
+        div.style.width = `${(400 / numSquares)}px`;
+        div.style.height = `${(400 / numSquares)}px`;
         div.id = "griditem";
-        div.style.width = sqsize;
-        div.style.height = sqsize;
+
         div.addEventListener("mouseenter", () => {
             div.style.backgroundColor = "black";
         });
